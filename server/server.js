cec37c4ba00cb685f4d6d3e0eb1818f88c0059e1
stormlycan <stormlycan@gmail.com>
2024-05-11 16:37:46 +0530

    created an apiroute

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import env from 'dotenv'
+import userRoutes from './routes/user.routes.js'
 env.config();
 
 mongoose.connect(process.env.MONGO_LINK).then(() => {
@@ -14,4 +15,7 @@ const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running at port ${PORT}`);
-})
+});
+
+
+app.use("/api/user", userRoutes);
