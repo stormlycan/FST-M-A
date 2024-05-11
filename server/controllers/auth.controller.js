6348fbf0375e0d322dd2c3c788e3b1ea821cd44d
stormlycan <stormlycan@gmail.com>
2024-05-11 23:02:52 +0530

    Created a signin api route for authenticationg

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -11,6 +12,27 @@ export const signup = async (req, res, next) => {
         res.status(201).json({
             message: "User created succefully"
         })
+    } catch (error) {
+        next(errorHandler(300, error.message));
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) {
+            return next(404, "user not found");
+        }
+        const validPassword = bcryptjs.compare(password, validUser.password);
+        if (!validPassword) {
+            return next(401, "Invalid credentials");
+        };
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "2h" });
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        const expiryData = new Date(Date.now() + 2*60*60*1000)
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryData})
+            .status(200).json(rest);
     } catch (error) {
         next(errorHandler(300, error.message));
     }
