e1043623211cd21d118fc517dfdcfdf7fe1333a6
stormlycan <stormlycan@gmail.com>
2024-05-11 14:28:23 +0530

    Navbar file name change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {Outlet} from 'react-router-dom'
-import Header from './components/Header'
+import Navbar from "./components/Navbar";
 
 
 export default function App() {
   return <>
-  <Header />
+  <Navbar />
   <Outlet />
   </>
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { NavLink} from 'react-router-dom'
-export default function Header() {
+export default function Navbar() {
   return (
     <div className='bg-slate-200'>
-      <div className='flex justify-between items-center text-xl mx-auto p-10'>
+      <div className='flex justify-between items-center text-xl mx-auto p-4'>
         <NavLink to="/"><h1 className='font-bold'>Auth App</h1></NavLink>
         <ul className='flex gap-4'>
           <NavLink to="/"><li>Home</li></NavLink>
